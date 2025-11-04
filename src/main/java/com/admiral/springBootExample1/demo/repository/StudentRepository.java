95c8a4b75a581a303be24d3f14eeabd588841639
Bhola Shankar <bhola.shankar@admiralindia.com>
2025-11-04 22:23:35 +0530

    last Commit

--- src/main/java/com/admiral/springBootExample1/demo/repository/StudentRepository.java
+++ src/main/java/com/admiral/springBootExample1/demo/repository/StudentRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository <StudentEntity, Long>{
     Optional<StudentEntity> findByPassportNo(String passportNo);
-
 }
